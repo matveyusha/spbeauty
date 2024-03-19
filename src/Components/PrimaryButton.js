53bb57b6d083ce34577b7addd3e2b31424f4f735
matveyusha <163291048+matveyusha@users.noreply.github.com>
2024-03-19 12:10:30 +0000

    выполнила правки

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -5,18 +5,21 @@ import styled from "styled-components";
 
 function PrimaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '144px' : props.minWidth; 
+    let minWidth = props.minWidth === undefined ? '217px' : props.minWidth; 
     const Button = styled.div`
-        background: #4B759E;
-        border: 1px solid #152842;
-        box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
+        background: #3B82F6;
         min-width: ${minWidth};
         color: white;
+        border-radius: 24px;
+        padding: 8px, 16px, 8px, 16px;
+        gap: 16px:
         &:hover {
-            background: linear-gradient(180deg, #4D9FF0 0%, #36699D 100%);
+            background: #0F172A;
+            opacity: 60%;
         }
         &:active {
-            background: #152842;
+            background: #E2E8F0;
+            opacity: 40%;
         }
     `;
     return (
--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -5,16 +5,24 @@ function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
     const Button = styled.div`
-        background: #F2F2F2;
-        border: 1px solid #DFDFDF;
-        box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
+        background: #FFFFFF;
+        border: 1px solid #64748B;
+        border-radius: 24px;
+        padding: 8px, 16px, 8px, 16px;
+        gap: 16px:
         min-width: ${minWidth};
         color: #152842;
         &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+            background: #FFFFFF;
+            opacity: 80%;
+        }
+        &:pressed {
+            background: #FFFFFF;
+            opacity: 60%;
         }
         &:active {
-            background: #DFDFDF;
+            background: #F8FAFC;
+            opacity: 40%;
         }
     `;
     return (
